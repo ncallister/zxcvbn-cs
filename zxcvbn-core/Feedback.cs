2e680287ca64bde8ee5366a17ac0d2af7a11d8ea
Thomas Mijieux <tmijieux@users.noreply.github.com>
2022-01-22 01:22:59 +0100

    Bug Fixes (#32)
    * fixes result.Score not setup with correct value before calling GetFeedback
    
    fixes trichards57/zxcvbn-cs#31
    
    * fix problem condition logic because of invalid nesting of condition in feedback
    
    fixes trichards57/zxcvbn-cs#29
    
    * fix crash when calling EvaluatePassword with empty string
    
    fixes trichards57/zxcvbn-cs#30

--- zxcvbn-core/Feedback.cs
+++ zxcvbn-core/Feedback.cs
@@ -75,9 +75,9 @@ namespace Zxcvbn
                     else
                         warning = "This is a very common password";
                 }
-                else
+                else if (match.GuessesLog10 <= 4)
                 {
-                    warning = "This is similar to a commonly used password.";
+                    warning = "This is similar to a commonly used password";
                 }
             }
             else if (match.DictionaryName == "english" && isSoleMatch)
